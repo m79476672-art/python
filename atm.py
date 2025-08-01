8f9946874d624a79d9633f1ed2bdcd1627985dd4
m79476672-art <m79476672@gmail.com>
2025-08-01 14:04:15 +0530

    atm.py

---
+++ atm.py
@@ -0,0 +1,6 @@
+ATM=int(input("Enter a  number:"))
+if "Deposit or credit" :
+    print("Enter the pin no")
+    print("Enter the amount")
+    print("credit or deposit")
+    print("take the cash")
--- hellowolrd.py
+++
@@ -1,7 +0,0 @@
-ATM=int(input("Enter a  number:"))
-if ("Insert a card"):
-elif("Enter the pin no"):
-elif("Enter the amount"):
-elif("credit or deposit"):
-else:
-    print("take the cash")
